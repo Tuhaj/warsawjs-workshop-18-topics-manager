740647b02bd68ffa4be148021c8be045c00f09bb
Piotr Zientara <314zientara@gmail.com>
2018-03-17 17:22:19 +0100

    Add redirect url

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-'use strict';
-require('bulma');
-const hello = require('hellojs');
+'use strict'
+require('bulma')
+const hello = require('hellojs')
 
 hello.init({
-  'github': '8fa50de40d90f1f92c96'
-});
+  github: '8fa50de40d90f1f92c96'
+})
 
+const github = hello('github');
 
-const githubLogin = document.querySelector('#GitHub-login');
+const githubLogin = document.querySelector('#GitHub-login')
 
 githubLogin.addEventListener('click', () => {
-  hello('github').login().then(function() {
-    alert('You are signed in to GitHub');
-  }, function(e) {
-    alert('Signin error: ' + e.error.message);
-  });
+  github.login().then(function () {
+    console.log('You are signed in to GitHub')
+  }, function (e) {
+    console.log('Signin error: ' + e.error.message)
+  })
 })
