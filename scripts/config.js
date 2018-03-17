b712d2b9b71d7ff8776d538abab2c488c5166c51
Piotr Zientara <314zientara@gmail.com>
2018-03-17 20:48:36 +0100

    Separate envs for local

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  github: '8fa50de40d90f1f92c96',
-  redirect_uri: 'http://localhost:1234'
+  localhost: 'http://localhost:1234/',
+  local: {
+    github: '8fa50de40d90f1f92c96',
+    redirect_uri: 'http://localhost:1234'
+  },
+  production: {
+    github: '8fa50de40d90f1f92c96',
+    redirect_uri: 'http://localhost:1234'
+  }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -2,29 +2,28 @@
 
 require('bulma')
 const hello = require('hellojs')
-const config = require('./config.js')
+const appConfig = require('./config.js')
+let config = null
 
-let userId = null
-let userAvatar = null
+config = window.location.href === appConfig.localhost ? appConfig.local : appConfig.production
+
 hello.init({
-  github: config.github,
+  github: config.github
 }, {
   redirect_uri: config.redirect_uri
 })
 
-const github = hello('github');
+const github = hello('github')
 
 const githubLogin = document.querySelector('#GitHub-login')
 const userName = document.querySelector('#user-panel')
 githubLogin.addEventListener('click', (e) => {
   e.preventDefault()
   github.login().then(function () {
-    return github.api('/me');
-  })
-  .then(function(user) {
+    return github.api('/me')
+  }).then(function (user) {
     userName.textContent = `Hello ${user.login}`
-    userAvatar = user.avatar
-    userId = user.id
+    window.localStorage.setItem('userId', user.id)
   }, function (e) {
     console.log('Signin error: ' + e.error.message)
   })
