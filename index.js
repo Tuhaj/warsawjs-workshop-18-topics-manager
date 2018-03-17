494d4ec1508129db0a3967f4e30034d3d542ece6
Piotr Zientara <314zientara@gmail.com>
2018-03-17 18:32:59 +0100

    Wip

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ require('bulma')
 const hello = require('hellojs')
 
 hello.init({
-  github: '8fa50de40d90f1f92c96'
+  github: '8fa50de40d90f1f92c96',
+}, {
+  redirect_uri: 'http://localhost:1234'
 })
 
 const github = hello('github');
@@ -11,9 +13,15 @@ const github = hello('github');
 const githubLogin = document.querySelector('#GitHub-login')
 
 githubLogin.addEventListener('click', () => {
+
   github.login().then(function () {
+    return github.api('/me');
+  })
+  .then(function() {
+    debugger
     console.log('You are signed in to GitHub')
   }, function (e) {
+    debugger
     console.log('Signin error: ' + e.error.message)
   })
 })
